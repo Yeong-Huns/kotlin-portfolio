4ac324ab056c84fae102877a6e51a7bbf3c57cd0
Yeong-huns <vosxja1@gmail.com>
2025-03-05 23:55:45 +0900

    프로젝트 환경 변수 설정

---
+++ src/main/kotlin/com/yeonghun/portfolio/KotlinPortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.yeonghun.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPortfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinPortfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/yeonghun/portfolio/KotlinPortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yeonghun.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

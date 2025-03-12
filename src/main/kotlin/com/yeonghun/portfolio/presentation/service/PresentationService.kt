42670db75ee0d2ea3f3c4d5f07cfce9e1aeb5953
Yeong-huns <vosxja1@gmail.com>
2025-03-12 23:37:34 +0900

    서비스 개발

--- src/main/kotlin/com/yeonghun/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/yeonghun/portfolio/presentation/service/PresentationService.kt
@@ -1,6 +1,12 @@
 package com.yeonghun.portfolio.presentation.service
 
+import com.yeonghun.portfolio.presentation.dto.IntroductionDTO
+import com.yeonghun.portfolio.presentation.dto.LinkDTO
+import com.yeonghun.portfolio.presentation.dto.ProjectDTO
+import com.yeonghun.portfolio.presentation.dto.ResumeDTO
+import com.yeonghun.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 /**
  *packageName    : com.yeonghun.portfolio.presentation.service
@@ -13,5 +19,37 @@ import org.springframework.stereotype.Service
  * 2025-03-12        Yeong-Huns       최초 생성
  */
 @Service
-class PresentationService {
+class PresentationService (
+    private val presentationRepository: PresentationRepository
+){
+    @Transactional(readOnly = true)
+    fun getIntroductions():List<IntroductionDTO> {
+        val introduction = presentationRepository.getActiveIntroductions()
+        return introduction.map(::IntroductionDTO)
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks():List<LinkDTO>{
+        val links = presentationRepository.getActiveLinks()
+        return links.map(::LinkDTO)
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects():List<ProjectDTO>{
+        val projects = presentationRepository.getActiveProjects()
+        return projects.map(::ProjectDTO)
+    }
 }

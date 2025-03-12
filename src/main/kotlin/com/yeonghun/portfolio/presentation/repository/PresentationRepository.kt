b1a40a84f480c9677c0393cd063a8a8f661c906f
Yeong-huns <vosxja1@gmail.com>
2025-03-12 23:32:05 +0900

    리포지토리 개발

--- src/main/kotlin/com/yeonghun/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/yeonghun/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,5 +1,18 @@
 package com.yeonghun.portfolio.presentation.repository
 
+import com.yeonghun.portfolio.domain.entity.Achievement
+import com.yeonghun.portfolio.domain.entity.Experience
+import com.yeonghun.portfolio.domain.entity.Introduction
+import com.yeonghun.portfolio.domain.entity.Link
+import com.yeonghun.portfolio.domain.entity.Project
+import com.yeonghun.portfolio.domain.entity.Skill
+import com.yeonghun.portfolio.domain.repository.AchievementRepository
+import com.yeonghun.portfolio.domain.repository.ExperienceRepository
+import com.yeonghun.portfolio.domain.repository.IntroductionRepository
+import com.yeonghun.portfolio.domain.repository.LinkRepository
+import com.yeonghun.portfolio.domain.repository.ProjectQueryDslRepository
+import com.yeonghun.portfolio.domain.repository.ProjectRepository
+import com.yeonghun.portfolio.domain.repository.SkillRepository
 import org.springframework.stereotype.Repository
 
 /**
@@ -13,5 +26,29 @@ import org.springframework.stereotype.Repository
  * 2025-03-12        Yeong-Huns       최초 생성
  */
 @Repository
-class PresentationRepository {
+class PresentationRepository (
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectQueryDslRepository: ProjectQueryDslRepository,
+    private val experienceRepository: ExperienceRepository,
+){
+    fun getActiveAchievements(): List<Achievement> =
+        achievementRepository.findAllByIsActive(true)
+
+    fun getActiveExperiences(): List<Experience> =
+        experienceRepository.findAllByIsActive(true)
+
+    fun getActiveIntroductions(): List<Introduction> =
+        introductionRepository.findAllByIsActive(true)
+
+    fun getActiveLinks(): List<Link> =
+        linkRepository.findAllByIsActive(true)
+
+    fun getActiveProjects(): List<Project> =
+        projectQueryDslRepository.findAllByIsActive(true)
+
+    fun getActiveSkills(): List<Skill> =
+        skillRepository.findAllByIsActive(true)
 }
